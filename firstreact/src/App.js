95e72244dfccbfaa6b925ef7d8208a209c85fc0c
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 12:42:40 +0700

    Basic variable
    Basic variable

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
 
 function App() {
+  const fullname = 'ศุภชัย สมพานิช';
   return (
-    <div>
-      <Header title="Thai vb.net Core" />
-      <div className="App">
-        <h1>Hello Main Component</h1>
-      </div>
-    </div>
+  <p>ชื่อ-สกุล : {fullname}</p>
   );
 }
 export default App;
