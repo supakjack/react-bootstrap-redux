15927b79070fe7730768a889611a433fc1f649bf
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 12:31:58 +0700

    New Header Component
    New Header Component

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <table border="1">
-        <tbody>
-          <tr>
-            <td>รหัสสินค้า</td>
-            <td>การใช้งานภาษา C#</td>
-            <td>335</td>
-          </tr>
-          <tr>
-            <td>001</td>
-            <td>พัฒนาระบบฐานข้อมูล Mongo DB ด้วย MEAN Stack</td>
-            <td>299</td>
-          </tr>
-          <tr>
-            <td>002</td>
-            <td>Basic ASP.NET Core MVC</td>
-            <td>379</td>
-          </tr>
-        </tbody>
-      </table>
+      <Header />
+      <h1>Hello Main Component</h1>
     </div>
   );
 }
