08bd8f8040c77f1decfe984a9822ae71e55cfbab
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 13:49:17 +0700

    class + function component
    class + function component

--- class-component/src/App.js
+++ class-component/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Home from './Home'
 
 export class App extends Component {
   render() {
     return (
       <div>
         <h1>สวัสดี คลาสคอมโพเนน</h1>
+        <Home />
       </div>
     )
   }
---
+++ class-component/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h2>สวัสดีฟังก์ชันคอมโพเนน</h2>
+        </div>
+    )
+}
+
+export default Home
