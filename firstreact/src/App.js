66fc53a520808c67412c9e58099dbf6e7fe19c46
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 12:44:18 +0700

    Sum variable
    Sum variable

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const fullname = 'ศุภชัย สมพานิช';
-  return (
-  <p>ชื่อ-สกุล : {fullname}</p>
-  );
+  const num1 = 100;
+  const num2 = 200;
+  return <p>ผลบวก : {num1+num2}</p>;
 }
 export default App;
