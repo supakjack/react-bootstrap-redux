36b04b5f86d4343e0b7c05c613c4f70b7c9d19c8
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 14:02:56 +0700

    setState in class
    setState in class

--- class-component/src/App.js
+++ class-component/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import Home from './Home';
 
 export class App extends Component {
-  render() {
-    const txtStyle = {
-      color: 'white',
-      backgroundColor: 'gray'
+  constructor() {
+    super();
+
+    this.state = {
+      data: 0
     };
+  }
+
+  render() {
     return (
       <div>
-        <h1 style={txtStyle}>สวัสดี คลาสคอมโพเนน</h1>
-        <Home name="My Mother Home" address="Bangkok" />
+        <p>ข้อมูลปัจจุบัน : {this.state.data}</p>
+        <button onClick={() => this.setState({ data: this.state.data + 1 })}>
+          คลิก
+        </button>
       </div>
     );
   }
