5344249c0f2e34650dd100ca5923fbd0bb45bd74
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 12:25:05 +0700

    Table
    Table

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react world</h1>
+      <table border="1">
+        <tbody>
+          <tr>
+            <td>รหัสสินค้า</td>
+            <td>การใช้งานภาษา C#</td>
+            <td>335</td>
+          </tr>
+          <tr>
+            <td>001</td>
+            <td>พัฒนาระบบฐานข้อมูล Mongo DB ด้วย MEAN Stack</td>
+            <td>299</td>
+          </tr>
+          <tr>
+            <td>002</td>
+            <td>Basic ASP.NET Core MVC</td>
+            <td>379</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
