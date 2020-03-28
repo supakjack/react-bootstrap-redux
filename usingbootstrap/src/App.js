b70a8c93ebf24b5dcc31ce8241ba982f7528314c
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-28 13:20:02 +0700

    useEffect
    useEffect

--- usingbootstrap/src/App.js
+++ usingbootstrap/src/App.js
@@ -1,16 +1,27 @@
-import React , { useState} from 'react'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data , setData] = useState(0);
+  const [data, setData] = useState(0);
+  const [txt, setTxt] = useState('Supak Jack');
+  useEffect(() => {
+    console.log({ data });
+    console.log({ txt });
+  });
+
   return (
     <div>
-      <p>
-        ข้อมูลปัจจุบัน : {data}
-      </p>
-
-      <button onClick={()=> setData(data+1)}>คลิก</button>
+      <p>ข้อมูลปัจจุบัน : {data}</p>
+      <p>ชื่อปัจจุบัน : {txt}</p>
+      <button
+        onClick={() => {
+          setData(data + 1);
+          setTxt('สุภัค แจ๊ค');
+        }}
+      >
+        คลิก
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
